1527b130ef311fbb2fb4324c0da3fc527e121541
Kowabongaa <158025851+Kowabongaa@users.noreply.github.com>
2025-01-22 22:11:53 +0700

    a

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,6 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     loop: true,
+    autoplay: {
+      delay: 5000, 
+      disableOnInteraction: false, // Keep autoplay running even after user interaction
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -10,3 +14,4 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+  
