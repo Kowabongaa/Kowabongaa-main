e4684c48a0dfc6434c453e488d3bdbbfb86ecfc5
Kowabongaa <158025851+Kowabongaa@users.noreply.github.com>
2025-01-22 22:05:01 +0700

    a

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,16 +1,12 @@
-const slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
-
-function showNextSlide() {
-  // Remove the active class from the current slide
-  slides[currentIndex].classList.remove('active');
-  
-  // Increment the index (loop back to 0 if at the end)
-  currentIndex = (currentIndex + 1) % slides.length;
-  
-  // Add the active class to the next slide
-  slides[currentIndex].classList.add('active');
-}
-
-// Automatically change slide every 5 seconds
-setInterval(showNextSlide, 5000);
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
